cbe8daf72ab368d5595e72dd19ccbbdd254da619
Young.Park <kasugare@gmail.com>
2018-09-10 13:44:50 +0900

    Add: added install.sh

---
+++ libs/install.sh
@@ -0,0 +1,61 @@
+tar zxvf certifi-2018.8.24.tar.gz
+python ./certifi-2018.8.24/setup.py install
+
+tar zxvf docopt-0.6.2.tar.gz
+python ./docopt-0.6.2/setup.py install
+
+tar zxvf idna-2.7.tar.gz
+python ./idna-2.7/setup.py install
+
+tar zxvf query_string-1.0.2.tar.gz
+python ./query_string-1.0.2/setup.py install
+
+tar zxvf chardet-3.0.4.tar.gz
+python ./chardet-3.0.4/setup.py install
+
+tar zxvf get-1.0.3.tar.gz
+python ./get-1.0.3/setup.py install
+
+tar zxvf post-1.0.2.tar.gz
+python ./post-1.0.2/setup.py install
+
+tar zxvf six-1.11.0.tar.gz
+python ./six-1.11.0/setup.py install
+
+tar zxvf colorama-0.3.9.tar.gz
+python ./colorama-0.3.9/setup.py install
+
+tar zxvf public-1.0.3.tar.gz
+python ./public-1.0.3/setup.py install
+
+tar zxvf request-1.0.2.tar.gz
+python ./request-1.0.2/setup.py install
+
+tar zxvf urllib3-1.23.tar.gz
+python ./urllib3-1.23/setup.py install
+
+tar zxvf requests-2.19.1.tar.gz
+python ./requests-2.19.1/setup.py install
+
+tar zxvf rainbow_logging_handler-2.2.2.zip
+python ./rainbow_logging_handler-2.2.2/setup.py install
+
+tar zxvf hdfs-2.1.0.tar.gz
+python ./hdfs-2.1.0/setup.py install
+
+
+rm -rf certifi-2018.8.24
+rm -rf docopt-0.6.2
+rm -rf idna-2.7
+rm -rf query_string-1.0.2
+rm -rf requests-2.19.1
+rm -rf chardet-3.0.4
+rm -rf get-1.0.3
+rm -rf post-1.0.2
+rm -rf rainbow_logging_handler-2.2.2
+rm -rf six-1.11.0
+rm -rf colorama-0.3.9
+rm -rf hdfs-2.1.0
+rm -rf public-1.0.3
+rm -rf request-1.0.2
+rm -rf urllib3-1.23
